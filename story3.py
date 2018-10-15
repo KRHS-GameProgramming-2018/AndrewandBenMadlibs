e66271aae9911c7298714ba4be04d65825545b66
Andrew Lagos <andrew.lagos@kearsarge.org>
2018-10-15 07:56:12 -0400

    finished story 3

--- story3.py
+++ story3.py
@@ -1,17 +1,15 @@
 from getInput import *
 
 def playMadlibs():
-    bird1 = getWord("Enter a bird")
-    numb1 = getWord("Enter a number")
-    verb1 = getWord("Enter a verb")
+    numb1 = getWord("Enter a Number: ")
+    bird1 = getWord("Enter a bird: ")
+    name1 = getWord("Enter a Name")
     adjective1 = getWord("Enter an adjective")
-    vechile = getWord("enter a vechile")
-    communicationsdevice1 = getWord("Enter a communications device")
-    friend2 = getWord("enter a name")
+    vechile1 = getWord("enter a vechile")
+    treetype1 = getWord("Enter a communications device")
+    color1 = getWord("enter a color")
     adj2 = getWord("Enter an adjective")
-    
-    
-    
+    month1 = getWord("Enter a month")
     
     output = ""
     output += "Wow what a long walk"
@@ -20,6 +18,16 @@ def playMadlibs():
     output += "Wow I think I see a"+bird1
     output += "Thats so cool"
     output += "wow this sunset sure is"+adj1
-    output += "
-
+    output += " It really is"
+    output += "I should invite my friend,"+name1
+    output += "I think he would really like it"
+    output += "I wonder where I parked my" +vechile1
+    output += "Its " +color1
+    output += "paint ahould make it stand out amoung these" +Treetype1
+    output += "Yeah these trees sure are" +adj2
+    output += "Wow what a great day in" +month1
+    
+    
+    
+    
     
